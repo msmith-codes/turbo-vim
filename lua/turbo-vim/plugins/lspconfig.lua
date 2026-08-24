8be9cef48573984dcb05127a52d759b6a02f70d0
msmith-codes <msmith.softwaredev@gmail.com>
2026-08-24 19:58:53 -0400

    updated lspconfig

--- lua/turbo-vim/plugins/lspconfig.lua
+++ lua/turbo-vim/plugins/lspconfig.lua
@@ -16,6 +16,8 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            vim.lsp.enabled('cmake')
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         end
